a2bc123023f905560729bd74072193c21b0a5522
aji <mancode700@gmail.com>
2025-02-11 12:31:23 +0700

    ADD: DOCKERFILE

--- config/app.php
+++ config/app.php
@@ -51,9 +51,9 @@ return [
     | the application so that it's available within Artisan commands.
     |
     */
-    'url' => env('APP_URL', 'https://company-profile-production-69d6.up.railway.app'),
+    'url' => env('APP_URL', 'https://website-hrd-production.up.railway.app/'),
 
-    'asset_url' => env('ASSET_URL', 'https://company-profile-production-69d6.up.railway.app'),
+    'asset_url' => env('ASSET_URL', 'https://website-hrd-production.up.railway.app/'),
 
     /*
     |--------------------------------------------------------------------------
