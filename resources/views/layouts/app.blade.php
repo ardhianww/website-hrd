1366a747196ab2c00ba252bb35f58b15f19d5306
Aji Maulana <mancode700@gmail.com>
2025-02-13 14:10:03 +0700

    dark mode function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -184,14 +184,33 @@
         </div>
     </div>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg {{ request()->cookie('theme') === 'dark' ? 'navbar-dark bg-dark' : 'navbar-light bg-light' }}">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+                        </li>
+                    @endauth
+                </ul>
+                
+                <!-- Dark Mode Toggle -->
+                <div class="d-flex align-items-center me-3">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="darkModeToggle" {{ request()->cookie('theme') === 'dark' ? 'checked' : '' }}>
+                        <label class="form-check-label" for="darkModeToggle">Dark Mode</label>
+                    </div>
+                </div>
+
+                <ul class="navbar-nav">
                     @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
